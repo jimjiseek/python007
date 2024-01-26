e1aad5efbe9e85e81911c81e4093e4c320f44fef
jimjiseek <157090844+jimjiseek@users.noreply.github.com>
2024-01-26 16:15:07 +0800

    Update mainprog.py with employee003.

--- mainprog.py
+++ mainprog.py
@@ -3,6 +3,7 @@ from companyEmp import Employee
 
 employee001 = Employee("Jane", "Tan", "M00123")   #create an instance of Employee as object employee001
 employee002 = Employee("Jim", "Wong", "M00555")
+employee003 = Employee("Peter", "Lim", "M00558")
 
 print(employee001.first_name, employee001.last_name, employee001.emp_number)
-employee002.print_emp_info
+employee003.print_emp_info
