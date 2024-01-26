e1cac79c230fbe06a0987612f8ee3f3e40badad1
jimjiseek <157090844+jimjiseek@users.noreply.github.com>
2024-01-26 16:12:32 +0800

    Adding in the first file for employee project

---
+++ mainprog.py
@@ -0,0 +1,8 @@
+
+from companyEmp import Employee
+
+employee001 = Employee("Jane", "Tan", "M00123")   #create an instance of Employee as object employee001
+employee002 = Employee("Jim", "Wong", "M00555")
+
+print(employee001.first_name, employee001.last_name, employee001.emp_number)
+employee002.print_emp_info
